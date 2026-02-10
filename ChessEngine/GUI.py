9b1454ed80b984c6f006aced9c2c56a47c4acd94
Nic Long <longnic550@gmail.com>
2026-02-10 06:17:24 -0700

    some pieces on board now

--- ChessEngine/GUI.py
+++ ChessEngine/GUI.py
@@ -1,41 +1,79 @@
 import pygame
+import math
 
 #a dictionary that holds every square on our board. data looks like:
-#a : [(Square1,Square1Color),(Square2,Square2Color)...]
-#b : [(Square9,Square9Color),(Square10,Square10Color)...]
+#0 : [(Square1,Square1Color),(Square2,Square2Color)...]
+#1 : [(Square9,Square9Color),(Square10,Square10Color)...]
 #.
 #.
 #.
 boardPositions = {}
 
+pieceAssets = []
+fenString = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR"
+
 #draw the chess board
 def build_board():
     boardPositions.clear() 
     #(x,y) offset for board
-    boardOffset = (250,0)
+    boardOffset = (167,0)
     xMax: int = 8
     yMax: int = 8
-    cellSize: int = 90
-    fileNames = ["a","b","c","d","e","f","g","h"]
-    colors = ["grey","black"]
-    for xPos in range(0,xMax):
+    cellSize: int = 60
+    colors = ["DarkSeaGreen","DarkGreen"]
+    for yPos in range(0,xMax):
         currentFile = []
-        for yPos in range(0,yMax):
+        for xPos in range(0,yMax):
             square = pygame.Rect((xPos*cellSize)+boardOffset[0],(yPos*cellSize)+boardOffset[1],cellSize,cellSize)
             tileColor = colors[(xPos+yPos)%2]
             currentFile.append((square,tileColor))
-        #reverse the list so it matches with expected chess notation (a8 is the top left corner)
-        currentFile.reverse()
-        boardPositions[fileNames[xPos]] = currentFile
+        boardPositions[yPos+1] = currentFile
+
+
+def load_assets():
+    allAssets = ["Assets\WhitePawn.png","Assets\BlackPawn.png"]
+    for assetPath in allAssets:
+        asset = pygame.image.load(assetPath).convert_alpha()
+        pieceAssets.append(asset)
+
+
+def get_cell_rect(row,col):
+    pieceCell = boardPositions[int(row)][int(col)][0]
+    return pieceCell
+
+#reading the FEN string to create the board.
+def place_pieces(screen):
+    rowNum = 1
+    colNum = 0
+    for i in range(len(fenString)):
+        currentChar = fenString[i]
+        if currentChar == "/":
+            rowNum += 1
+            colNum = 0
+            continue
+        #if we read a number, these are empty cells and we can skip over them.
+        if currentChar.isnumeric():
+            #-1 because we add 1 at every iteration
+            colNum += int(currentChar)
+            continue
+        elif currentChar == 'P':
+            pieceCell = get_cell_rect(rowNum,colNum)
+            screen.blit(pieceAssets[0],pieceCell)
+        elif currentChar == 'p':
+            pieceCell = get_cell_rect(rowNum,colNum)
+            screen.blit(pieceAssets[1],pieceCell)
+        colNum += 1
+
 
 
 # pygame setup
 def main():
     pygame.init()
-    screen = pygame.display.set_mode((1280, 720))
+    screen = pygame.display.set_mode((853, 480))
     clock = pygame.time.Clock()
     running = True
     build_board()
+    load_assets()
     while running:
         # poll for events
         # pygame.QUIT event means the user clicked X to close your window
@@ -47,16 +85,17 @@ def main():
         screen.fill("black")
 
         # RENDER GAME
-        
+        #start with the board
         for key, value in boardPositions.items():
             for rectInfo in value:
                 screen.fill(rectInfo[1],rectInfo[0])
 
-
+        #then the pieces
+        place_pieces(screen)
         # flip() the display to put your work on screen
         pygame.display.flip()
 
-        clock.tick(60)  # limits FPS to 60
+        clock.tick(20)  # limits FPS to 20
 
     pygame.quit()
 
